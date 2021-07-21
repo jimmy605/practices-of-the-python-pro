20efc68fc46d35050cc3a6007184f2a4b97167ad
jimmy605 <605jimmy@gmail.com>
2021-07-21 19:38:40 +1000

    Use book code as mine does not seem to be working now?

--- My_Code/ch06/commands.py
+++ My_Code/ch06/commands.py
@@ -1,37 +1,43 @@
-import sys 
-from data_base import DataBaseManager
+import sys
 from datetime import datetime
 
-db = DataBaseManager('bookmarks.db')
+from database import DatabaseManager
+
+db = DatabaseManager('bookmarks.db')  # <1>
+
 
 class CreateBookmarksTableCommand:
-    def execute(self):
-        db.create_table('bookmarks', {
+    def execute(self):  # <2>
+        db.create_table('bookmarks', {  # <3>
             'id': 'integer primary key autoincrement',
             'title': 'text not null',
             'url': 'text not null',
             'notes': 'text',
-            'date_added': 'text not null'
+            'date_added': 'text not null',
         })
-    
+
+
 class AddBookmarkCommand:
     def execute(self, data):
-        data['data_added'] = datetime.utcnow().isoformat()
-        db.add('bookmarks', data)
-        return 'Bookmarks added!'
+        data['date_added'] = datetime.utcnow().isoformat()  # <1>
+        db.add('bookmarks', data)  # <2>
+        return 'Bookmark added!'  # <3>
+
 
 class ListBookmarksCommand:
-    def __init__(self, order_by='date_added'):
+    def __init__(self, order_by='date_added'):  # <1>
         self.order_by = order_by
-    
+
     def execute(self):
-        return db.select('bookmarks', order_by=self.order_by).fetchall()
+        return db.select('bookmarks', order_by=self.order_by).fetchall()  # <2>
+
 
 class DeleteBookmarkCommand:
     def execute(self, data):
-        db.delete('bookmarks', {'id': data})
+        db.delete('bookmarks', {'id': data})  # <1>
         return 'Bookmark deleted!'
+
 
 class QuitCommand:
     def execute(self):
-        sys.exit()
+        sys.exit()  # <1>
